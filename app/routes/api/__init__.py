105037ff004798c8b51d9b076a479d93b83e4515
ChanReMin <minhletran152003@gmail.com>
2024-12-18 14:11:42 +0700

    leminh: update record video with audio

--- app/routes/api/__init__.py
+++ app/routes/api/__init__.py
@@ -3,9 +3,11 @@ from app.routes.api.admin import api_admin_bp
 from app.routes.api.user import api_user_bp
 from app.routes.api.auth import api_auth_bp
 from app.routes.api.artist import api_artist_bp
+from app.routes.api.music import api_music_bp
 
 api_bp = Blueprint('api', __name__)
 api_bp.register_blueprint(api_admin_bp, url_prefix='/admin')
 api_bp.register_blueprint(api_auth_bp, url_prefix='/auth')
 api_bp.register_blueprint(api_user_bp, url_prefix='/user')
 api_bp.register_blueprint(api_artist_bp, url_prefix='/artist')
+api_bp.register_blueprint(api_music_bp, url_prefix='/music')
