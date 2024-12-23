04b5e56b42aa162ec30467b03744dbb60379e368
ChanReMin <minhletran152003@gmail.com>
2024-12-23 20:12:17 +0700

    leminh: add minor changes

--- app/static/js/auth/register.js
+++ app/static/js/auth/register.js
@@ -3,6 +3,10 @@ function register() {
     const username = $('#register-username').val();
     const email = $('#register-email').val();
     const DateOfBirth = $('#register-dob').val();
+    if (!dobValidate(DateOfBirth))
+    {
+        return;
+    };
     const password = $('#register-password').val();
 
     const data = {
@@ -12,7 +16,10 @@ function register() {
         password: password,
         date_of_birth: DateOfBirth,
     };
-
+    if (!emailValidation(email)) {
+        alert('Email không hợp lệ');
+        return;
+    };
     // Make the API call using Axios
     axios.post('/api/auth/register/create', data)
         .then((response) => {
@@ -34,4 +41,24 @@ function register() {
             console.error('Error register user:', error);
             alert('Lỗi đăng ký. Vui lòng thử lại.');
         });
+}
+function emailValidation(email){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+function dobValidate(dob){
+    if (!dob) {
+        alert('Ngày sinh không được để trống');
+        return False
+    }
+    const dobDate = new Date(dob);
+    const year = dobDate.getFullYear();
+    const month = dobDate.getMonth() + 1; // Months are zero-based
+    const day = dobDate.getDate();
+    const formattedDate = `${month}/${day}/${year}`;
+    if (new Date(formattedDate) > new Date()) {
+        alert('Ngày sinh không hợp lệ');
+        return False
+    }
+    return True;
 }
--- test/test.py
+++ test/test.py
@@ -1,14 +1,17 @@
+import os, sys;
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))));
 from mongoengine import connect
-
-connect('PBL6')
-
+from app.models.admin import Admin
+connect('karaokeDB')
 from app.models.user import User
-User(username='nhatminh', email='nhatminh@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh1', email='nhatminh1@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh2', email='nhatminh2@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh3', email='nhatminh3@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh4', email='nhatminh4@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh5', email='nhatminh5@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh6', email='nhatminh6@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh7', email='nhatminh7@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
-User(username='nhatminh8', email='nhatminh8@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh', email='nhatminh@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh1', email='nhatminh1@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh2', email='nhatminh2@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh3', email='nhatminh3@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh4', email='nhatminh4@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh5', email='nhatminh5@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh6', email='nhatminh6@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh7', email='nhatminh7@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh8', email='nhatminh8@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+# User(username='nhatminh8', email='nhatminh8@gmail.com', date_of_birth='2003-10-28').set_password('123456').save()
+Admin(username='admin', name='Admin').set_password('123456').save()
